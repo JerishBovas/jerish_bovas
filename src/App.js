3a301f60df9bf1487bfba556602e5477edfd936a
Jerish Bovas <jerishbradlyb@gmail.com>
2022-04-19 19:40:01 -0400

    partial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
 import './App.css';
+import Contact from './containers/Contact/Contact';
+import Projects from './containers/Projects/Projects';
+import Skills from './containers/Skills/Skills';
+import Summary from './containers/Summary/Summary';
+import logo from './logo_square.svg';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <nav class="navbar navbar-expand-lg">
+        <div class="container-fluid fs-4">
+          <a class="navbar-brand ms-5 d-flex" style={{ color: 'rgba(242, 245, 247, 0.90)'}} href="https://www.jerishbovas.com">
+            <img src={logo} style={{margin: 'auto'}} alt="logo_image" height="40rem"/>
+            <span style={{fontFamily: 'cursive',}} class="ms-3 fs-2">Jerish Bovas</span>
+          </a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav m-auto">
+              <li class="nav-item m-1">
+                <a class="nav-link" href="#summary">Summary</a>
+              </li>
+              <li class="nav-item m-1">
+                <a class="nav-link" href="#projects">Projects</a>
+              </li>
+              <li class="nav-item m-1">
+                <a class="nav-link" href="#skills">Skills</a>
+              </li>
+              <li class="nav-item m-1">
+                <a class="nav-link" href='#contact'>Contact</a>
+              </li>
+            </ul>
+            <ul class="navbar-nav ml-auto me-5">
+              <li class="nav-item me-3">
+                <a class="nav-link" target="_blank" rel='noreferrer' href="https://github.com/JerishBovas?tab=repositories"><i class="fa fa-brands fa-github"></i></a>
+              </li>
+              <li class="nav-item me-3">
+                <a class="nav-link" target="_blank" rel='noreferrer' href="https://www.linkedin.com/in/jerishbovas/"><i class="fa fa-brands fa-linkedin"></i></a>
+              </li>
+              <li class="nav-item me-3">
+                <a class="nav-link" target="_blank" rel='noreferrer' href="https://twitter.com/bovasjerish"><i class="fa fa-brands fa-twitter"></i></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" target="_blank" rel='noreferrer' href="https://www.youtube.com/channel/UChPApQMTIOvdRR9M7VMPxDw"><i class="fa fa-brands fa-youtube"></i></a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+      <Summary/>
+      <Projects/>
+      <Skills/>
+      <Contact/>
     </div>
   );
 }
---
+++ src/containers/Summary/Summary.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function Summary() {
+  return (
+    <div id='summary' style={{width: '100%', marginTop: '100px'}} className='container'>
+        <div class="m-5" style={{width: '60%', marginRight: 'auto'}}>
+            <p class="h1"><small>I'm</small> Jerish Bradly Bovas</p>
+            <p class="h4 text-muted">Computer Programmer Analyst</p>
+            <p class="lead font-weight-bold">Computer Programmer Analyst specialized in Web, Mobile, Desktop and Cross-Platform development using various frameworks like .NET, React.js, Angular, Android, iOS, React Native, Ionic, NodeJS, Express.js, Spring Boot, PHP etc. Interested in working with Algorithms, Artificial Intelligence, Machine Learning and implementing them in production applications. Excelled in Engineering DevOps and fully automating project deploy and management using Azure and GCP Cloud Services.</p>
+        </div>
+        <img src="https://giphy.com/embed/26tn33aiTi1jkl6H6" width="480" height="270"></img>
+    </div>
+  )
+}
