5071a37b2dcf21a64f6c80c6709d55d7b97c2285
Jerish Bovas <jerishbradlyb@gmail.com>
2022-05-01 13:09:15 -0400

    contact me btn fixed

--- src/containers/Summary/Summary.js
+++ src/containers/Summary/Summary.js
@@ -7,7 +7,7 @@ export default function Summary() {
             <p className="h1">Jerish Bradly Bovas</p>
             <p className="h3 mt-3 text-muted">Computer Programmer Analyst</p>
             <p style={{fontSize: 25}} className="mt-4 mb-4">Computer Programmer Analyst specialized in Web, Mobile, Desktop and Cross-Platform development using various frameworks like .NET, React.js, Angular, Android, iOS, React Native, Ionic, NodeJS, Express.js, Spring Boot, PHP etc. Interested in working with Algorithms, Artificial Intelligence, Machine Learning and implementing them in production applications. Excelled in Engineering DevOps and fully automating project deploy and management using Azure and GCP Cloud Services.</p>
-            <a href='#contact' id='contactmebtn'>Contact Me</a>
+            <a href='#contact' id='contactmebtn'><button>Contact Me</button></a>
         </div>
     </div>
   )
