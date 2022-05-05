be5569092fc357e2f8a8c3ac45ef0ed3745bde9f
Jerish Bovas <jerishbradlyb@gmail.com>
2022-05-05 12:06:08 -0400

    job title changed

--- src/containers/Summary/Summary.js
+++ src/containers/Summary/Summary.js
@@ -5,7 +5,7 @@ export default function Summary() {
     <div style={{width: '100%', paddingTop: 150, fontFamily: 'Varela Round'}} className='container'>
         <div className="m-5" style={{width: '70%', marginRight: 'auto'}}>
             <p className="h1">Jerish Bradly Bovas</p>
-            <p className="h3 mt-3 text-success">Computer Programmer Analyst</p>
+            <p className="h3 mt-3 text-success">Software Developer</p>
             <p style={{fontSize: 25}} className="mt-4 mb-4">Computer Programmer Analyst specialized in Web, Mobile, Desktop and Cross-Platform development using various frameworks like .NET, React.js, Angular, Android, iOS, React Native, Ionic, NodeJS, Express.js, Spring Boot, PHP etc. Interested in working with Algorithms, Artificial Intelligence, Machine Learning and implementing them in production applications. Excelled in Engineering DevOps and fully automating project deploy and management using Azure and GCP Cloud Services.</p>
             <a href='#contact' id='contactmebtn'><button>Contact Me</button></a>
         </div>
