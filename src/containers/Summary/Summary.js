018480b837676d6f879d4e727d4d801c7305b8f6
Jerish Bovas <jerishbradlyb@gmail.com>
2022-05-05 21:37:58 -0400

    summary updated

--- src/containers/Summary/Summary.js
+++ src/containers/Summary/Summary.js
@@ -6,7 +6,12 @@ export default function Summary() {
         <div className="m-5" style={{width: '70%', marginRight: 'auto'}}>
             <p className="h1">Jerish Bradly Bovas</p>
             <p className="h3 mt-3 text-success">Software Developer</p>
-            <p style={{fontSize: 25}} className="mt-4 mb-4">Computer Programmer Analyst specialized in Web, Mobile, Desktop and Cross-Platform development using various frameworks like .NET, React.js, Angular, Android, iOS, React Native, Ionic, NodeJS, Express.js, Spring Boot, PHP etc. Interested in working with Algorithms, Artificial Intelligence, Machine Learning and implementing them in production applications. Excelled in Engineering DevOps and fully automating project deploy and management using Azure and GCP Cloud Services.</p>
+            <p style={{fontSize: 25}} className="mt-4 mb-4">Software Developer with 2+ years of experience in Web, Mobile, Desktop and Cross-Platform
+development using various frameworks including .NET, React.js, Angular, Android, iOS, React Native, Ionic,
+NodeJS, Express.js, Spring Boot, PHP. Interested in working with Algorithms, Artificial Intelligence, and
+Machine Learning and implementing them in production applications. Excelled in Engineering DevOps and
+fully automating project deployment and management using Play Console, App Store Connect, Azure and
+GCP Cloud Services.</p>
             <a href='#contact' id='contactmebtn'><button>Contact Me</button></a>
         </div>
     </div>
